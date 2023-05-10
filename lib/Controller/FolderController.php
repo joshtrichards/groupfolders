4911524b3ca30399b6b29ac81ca8a27dd7f2f857
Maksim Sukharev <antreesy.web@gmail.com>
2023-05-10 11:01:20 +0200

    check in addition for folder existence at FolderManager
    Co-authored-by: Julius Härtl <jus@bitgrid.net>
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- lib/Controller/FolderController.php
+++ lib/Controller/FolderController.php
@@ -98,6 +98,10 @@ class FolderController extends OCSController {
 		if ($storageId === null) {
 			return new DataResponse([], Http::STATUS_NOT_FOUND);
 		}
+		$folder = $this->manager->getFolder($id, $storageId);
+		if ($folder === false) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
 		return null;
 	}
 
